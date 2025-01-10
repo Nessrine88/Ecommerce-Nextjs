26242e03c355ab3dbb8739adbd6dec9e99c37061
AbdulSalamAbro <abdulsalamabro@gmail.com>
2025-01-10 18:23:38 +0100

    wrap the checkout in suspense component

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,27 +1,38 @@
-"use client"
-import React from 'react'
-import {loadStripe} from '@stripe/stripe-js';
+"use client";
+
+import React, { Suspense } from 'react';
+import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
-import CheckoutForm from './_components/CheckoutForm'
+import CheckoutForm from './_components/CheckoutForm';
 import { useSearchParams } from 'next/navigation';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHER_KEY);
 
-const Checkout = () => {
-    const searchParams =useSearchParams();
+function CheckoutComponent() {
+    const searchParams = useSearchParams();
+    const amount = Number(searchParams.get("amount"));
+
     const options = {
         mode: 'payment',
         currency: "usd",
-        amount: Number(searchParams.get("amount"))*100
+        amount: amount * 100,
     };
 
     return (
         <div>
             <Elements stripe={stripePromise} options={options}>
-                <CheckoutForm amount={Number(searchParams.get("amount"))}/>
+                <CheckoutForm amount={amount} />
             </Elements>
         </div>
     );
 }
+
+const Checkout = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <CheckoutComponent />
+        </Suspense>
+    );
+};
 
 export default Checkout;
